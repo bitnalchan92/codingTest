7dac491730a7dd9b52611019d7f4a6452c3802c9
bitnalchan92 <bitnalchan92@gmail.com>
2025-05-26 17:29:33 +0900

    update

--- editor/src/Main.java
+++ editor/src/Main.java
@@ -1,22 +1,22 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public String solution(String word) {
-        String result = "NO";
-        String temp = new StringBuilder(word).reverse().toString();
-
-        if (word.equalsIgnoreCase(temp)) return "YES";
-
-        return result;
-    }
-
     public static void main(String[] args) throws IOException {
-        Main M = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String word = br.readLine();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        /**
+         * A : 낮에 올라갈 수 있는 높이
+         * B : 밤에 미끄러져 내려오는 높이
+         * V : 막대기의 높이
+         */
+        int A = Integer.parseInt(st.nextToken()), B = Integer.parseInt(st.nextToken()), V = Integer.parseInt(st.nextToken());
+        br.close();
 
-        System.out.println(M.solution(word));
+        int days = (int)Math.ceil(((double)(V - A) / (A - B)));
+
+        System.out.println(days + 1);
     }
 }
