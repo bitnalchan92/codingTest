42521d94d77076980051b7bd265b29a95e02eb6a
bitnalchan92 <bitnalchan92@gmail.com>
2025-06-18 16:20:40 +0900

    update

--- editor/src/Main.java
+++ editor/src/Main.java
@@ -2,36 +2,32 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    public int[] getScoreOrderArray(int n, int[] arr) {
-        int[] answer = new int[n];
-
-        for (int i = 0; i < n; i++) {
-            int cnt = 1;
-
-            for (int j = 0; j < n; j++) {
-                if (arr[j] > arr[i]) cnt++;
-            }
-
-            answer[i] = cnt;
+    public void printRelation(int a, int b) {
+        // 첫 번째 숫자가 두 번째 숫자의 약수라면 factor를, 배수라면 multiple을, 둘 다 아니라면 neither를 출력
+        if (b % a == 0) {
+            System.out.println("factor");
+        } else if (a % b == 0) {
+            System.out.println("multiple");
+        } else {
+            System.out.println("neither");
         }
-
-        return answer;
     }
 
     public static void main(String[] args) throws IOException {
-        Main M = new Main();
+        Main T = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int scoreCnt = Integer.parseInt(br.readLine());
-        int[] scoreArr = new int[scoreCnt];
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        while (true) {
+            String line = br.readLine();
+            if (line.equals("0 0")) break;
+
+            StringTokenizer st = new StringTokenizer(line);
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            T.printRelation(a, b);
+        }
+
         br.close();
-        for (int i = 0; i < scoreCnt; i++) {
-            scoreArr[i] = Integer.parseInt(st.nextToken());
-        }
-
-        for (int order : M.getScoreOrderArray(scoreCnt, scoreArr)) {
-            System.out.print(order + " ");
-        }
     }
 }
