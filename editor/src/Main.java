94c6263b4e41bfd03f22198e4e23bb484e2677d3
bitnalchan92 <bitnalchan92@gmail.com>
2025-06-19 14:18:22 +0900

    강의 수강

--- editor/src/Main.java
+++ editor/src/Main.java
@@ -1,33 +1,58 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public void printRelation(int a, int b) {
-        // 첫 번째 숫자가 두 번째 숫자의 약수라면 factor를, 배수라면 multiple을, 둘 다 아니라면 neither를 출력
-        if (b % a == 0) {
-            System.out.println("factor");
-        } else if (a % b == 0) {
-            System.out.println("multiple");
-        } else {
-            System.out.println("neither");
+    public int[][] getArray() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[][] arr = new int[N][N];
+
+        StringTokenizer st;
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
         }
+
+        br.close();
+
+        return arr;
+    }
+
+    int[] dx = {-1, 0, 1, 0};
+    int[] dy = {0, 1, 0, -1};
+
+    public int getTopCount(int[][] map) {
+        int result = 0;
+        int n = map.length;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                boolean isTop = true;
+
+                for (int k = 0; k < 4; k++) {
+                    int nx = i + dx[k]; // next direction X
+                    int ny = j + dy[k]; // next direction Y
+
+                    if (nx >= 0 && nx < n && ny >= 0 && ny < n && map[nx][ny] >= map[i][j]) {
+                        isTop = false;
+                        break;
+                    }
+                }
+
+                if (isTop) result++;
+            }
+        }
+
+        return result;
     }
 
     public static void main(String[] args) throws IOException {
         Main T = new Main();
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        while (true) {
-            String line = br.readLine();
-            if (line.equals("0 0")) break;
-
-            StringTokenizer st = new StringTokenizer(line);
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-
-            T.printRelation(a, b);
-        }
-
-        br.close();
+        int[][] map = T.getArray();
+        System.out.println(T.getTopCount(map));
     }
 }
