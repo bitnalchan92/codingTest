a7a4e7b697c66889cee95ceaed366db2570cf5e1
Banghakdong_Tobagee <155719581+bitnalchan92@users.noreply.github.com>
2025-12-12 16:09:11 +0900

    Update 두 개 뽑아서 더하기.py

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.py
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.py
@@ -1,17 +1,29 @@
+# def solution(numbers):
+#     answer = []
+    
+#     for i in range(len(numbers)): # 조합을 구하기 위해 순회
+#         for j in range(len(numbers)):
+#             # 서로 다른 인덱스의 두 수를 뽑아야 한다. 
+#             if i == j:
+#                 continue
+            
+#             sum = numbers[i] + numbers[j]
+#             answer.append(sum)
+    
+#     # set을 이용한 중복제거
+#     answer = list(set(answer))
+#     answer.sort()
+    
+#     return answer
+
 def solution(numbers):
     answer = []
     
     for i in range(len(numbers)): # 조합을 구하기 위해 순회
-        for j in range(len(numbers)):
-            # 서로 다른 인덱스의 두 수를 뽑아야 한다. 
-            if i == j:
-                continue
-            
-            sum = numbers[i] + numbers[j]
-            answer.append(sum)
+        for j in range(i+1, len(numbers)):
+            answer.append(numbers[i] + numbers[j])
     
     # set을 이용한 중복제거
-    answer = list(set(answer))
-    answer.sort()
+    answer = sorted(set(answer))
     
     return answer
