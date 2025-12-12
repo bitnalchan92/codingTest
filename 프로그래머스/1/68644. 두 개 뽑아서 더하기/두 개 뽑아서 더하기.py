f5e098eeee17f146c061891f2c52f60c8d14ff4b
Banghakdong_Tobagee <155719581+bitnalchan92@users.noreply.github.com>
2025-12-12 16:00:56 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 1.11 ms, Memory: 9.41 MB -BaekjoonHub

---
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.py
@@ -0,0 +1,17 @@
+def solution(numbers):
+    answer = []
+    
+    for i in range(len(numbers)): # 조합을 구하기 위해 순회
+        for j in range(len(numbers)):
+            # 서로 다른 인덱스의 두 수를 뽑아야 한다. 
+            if i == j:
+                continue
+            
+            sum = numbers[i] + numbers[j]
+            answer.append(sum)
+    
+    # set을 이용한 중복제거
+    answer = list(set(answer))
+    answer.sort()
+    
+    return answer
