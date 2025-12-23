3f1e3261ffa9eb83cb52e78dc8c84cb2e5588a50
Banghakdong_Tobagee <155719581+bitnalchan92@users.noreply.github.com>
2025-12-23 11:13:36 +0900

    [Bronze V] Title: A+B - 4, Time: 120 ms, Memory: 15872 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String line;
+        
+        while( (line = br.readLine()) != null ) {
+            StringTokenizer st = new StringTokenizer(line);
+            
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            bw.write((a+b) + "\n");
+        }
+        
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
