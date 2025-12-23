fa80427e644ff7e76a895c829288389f8cc7d509
Banghakdong_Tobagee <155719581+bitnalchan92@users.noreply.github.com>
2025-12-23 11:45:34 +0900

    [Bronze V] Title: 개수 세기, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.py
+++ 백준/Bronze/10807. 개수 세기/개수 세기.py
@@ -1,12 +1,8 @@
 import sys
 
-size = int(input())
-numbers = [int(n) for n in sys.stdin.readline().split()]
-findNumber = int(input())
+rl = sys.stdin.readline
+size = int(rl())
+numbers = list(map(int, rl().split()))
+findNumber = int(rl())
 
-result = 0
-for number in numbers:
-    if findNumber == number:
-        result += 1
-
-print(result)
+print(numbers.count(findNumber))
