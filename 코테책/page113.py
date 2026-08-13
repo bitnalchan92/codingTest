d3855c8e4eadcfa4e3e045305f1f03fb834bbf2a
Banghakdong_Tobagee <bitnalchan92@gmail.com>
2026-08-13 13:58:05 +0900

    update

---
+++ 코테책/page113.py
@@ -0,0 +1,8 @@
+def solution(numbers):
+    unique_list = list(set(numbers))
+    unique_list.sort(reverse=True)
+    return unique_list
+
+# TEST 코드 입니다. 주석을 풀고 실행시켜보세요
+print(solution([4, 2, 2, 1, 3, 4])) # 반환값 : [4, 3, 2, 1]
+print(solution([2, 1, 1, 3, 2, 5, 4])) # 반환값 : [5, 4, 3, 2, 1]
